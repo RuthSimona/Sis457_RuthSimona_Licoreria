5e1e0ad2eb832e9703c821bf50ba6b38a9ed202b
RuthSimona <ruthsimonacopachoque616@gmail.com>
2024-06-03 15:03:17 -0400

    Base de datos con la coneción a las 3 capas

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sis457_RuthSimona_Licoreria
---
+++ Sis457Licoreria/ClnLicoreria/ProductoCln.cs
@@ -0,0 +1,20 @@
+﻿using CadLicoreria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnLicoreria
+{
+    public class ProductoCln
+    {
+        public static List<paProductosListar_Result> ListarPa(string parametro)
+        {
+            using (var context = new LabLicoreriaEntities())
+            {
+                return context.paProductosListar(parametro).ToList();
+            }
+        }
+    }
+}
